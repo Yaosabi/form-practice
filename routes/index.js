9eadfbea4d205d263c6f1b9e67c2d1d981f8e62a
Yaosabi <lorenzoimurillo@outlook.com>
2021-11-18 21:24:15 -0800

    checkpoint 1

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
 });
+
+router.post('/results', function(req, res){
+  console.log(req.body.description);
+  console.log(req.body.agree);
+  console.log(req.body.icecream);
+  console.log(req.body.iceCreamOther);
+  res.render('index')
+})
 
 module.exports = router;
